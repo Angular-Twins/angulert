2b6ae2d34e67d0377a643742584249e5e6e04e73
Derek Hall <dsh8837@gmail.com>
2014-03-15 16:15:10 -0400

    working unit tests son

--- angulert.js
+++ angulert.js
@@ -5,37 +5,35 @@ angular.module('angulert', dependencies)
 
   var _config = {};
 
-  var $angulertProvider = {
-    $get: ['$rootScope', '$q', '$http', '$templateCache', '$controller',
-      function($rootScope, $q, $http, $templateCache, $controller) {
-        var _history = [];
-        var _alerts = [];
+  this.$get = ['$rootScope', '$q', '$http', '$templateCache', '$controller',
+    function($rootScope, $q, $http, $templateCache, $controller) {
+      var _history = [];
+      var _alerts = [];
 
-        var angulertService = {
-          addAlert: function(alert) {
+      var angulertService = {
+        addAlert: function(alert) {
 
-          },
-          deleteAlert: function(id) {
+        },
+        deleteAlert: function(id) {
 
-          },
-          getAlert: function(id) {
+        },
+        getAlert: function(id) {
 
-          },
-          updateAlert: function(alert) {
+        },
+        updateAlert: function(alert) {
 
-          },
-          getHistory: function() {
-            return _history;
-          },
-          deleteHistory: function() {
-            _history = [];
-          }
-        };
+        },
+        getHistory: function() {
+          return _history;
+        },
+        deleteHistory: function() {
+          _history = [];
+        }
+      };
 
-        return angulertService;
-      }
-    ]
-  };
+      return angulertService;
+    }
+  ]
 }])
 
 .directive('angulert', [function () {
--- test/unit/provider-spec.js
+++ test/unit/provider-spec.js
@@ -1,16 +1,28 @@
 'use strict';
 
 describe('$angulert provider', function() {
-  var $angulertProvider;
+  var angulertProvider;
 
   beforeEach(module('angulert'));
-  beforeEach(module(function(_$angulertProvider_) {
-    $angulertProvider = _$angulertProvider_;
+  beforeEach(module(function($angulertProvider) {
+    angulertProvider = $angulertProvider;
   }));
 
-  it('should exist', function() {
-    debugger;
-    expect(angular.isArray([])).toBeTruthy();
-    // expect($angulertProvider).toBe({});
+  it('should exist', inject(function($angulert) {
+    expect(angular).toBeDefined();
+    expect(angulertProvider).toBeDefined();
+    expect($angulert).toBeDefined();
+    expect($angulert.getHistory()).toEqual([]);
+  }));
+
+  describe('angulert interface', function() {
+    it('should have this public interface', inject(function($angulert) {
+      expect(angular.isFunction($angulert.addAlert)).toBe(true);
+      expect(angular.isFunction($angulert.deleteAlert)).toBe(true);
+      expect(angular.isFunction($angulert.getAlert)).toBe(true);
+      expect(angular.isFunction($angulert.updateAlert)).toBe(true);
+      expect(angular.isFunction($angulert.getHistory)).toBe(true);
+      expect(angular.isFunction($angulert.deleteHistory)).toBe(true);
+    }));
   });
 });
