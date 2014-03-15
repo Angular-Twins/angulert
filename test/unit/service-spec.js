458f18c6ef32153a12c1b044438b936fd6acc888
Derek Hall <dsh8837@gmail.com>
2014-03-15 19:46:06 -0400

    more tests for angulert service

--- test/unit/service-spec.js
+++ test/unit/service-spec.js
@@ -18,20 +18,53 @@ describe('angulert service', function() {
     }));
   });
 
+  it('should clear alerts', inject(function($injector) {
+    var $angulert = $injector.get('$angulert');
+    $angulert.addAlert({
+      message: 'Alert'
+    });
+    $angulert.clearAlerts();
+    expect($angulert.getAlerts().length).toEqual(0);
+  }));
+
   describe('adding alerts', function() {
     var $angulert;
 
-    beforeEach(inject(function($injector){
+    beforeEach(inject(function($injector) {
       $angulert = $injector.get('$angulert');
       $angulert.clearAlerts();
-      $angulert.addAlert({
-        message: 'Awesome Alert'
-      });
     }));
 
     it('should add an alert', function() {
+      $angulert.addAlert({
+        message: 'Awesome Alert'
+      });
       expect($angulert.getAlerts().length).toBe(1);
       expect($angulert.getAlerts()[0].message).toBe('Awesome Alert');
+    });
+
+    it('should add a success alert', function() {
+      var alert = { message: 'Success alert'};
+      $angulert.success(alert);
+      expect(alert.classes).toEqual(['success']);
+    });
+
+    it('should add a warn alert', function() {
+      var alert = { message: 'Warn alert'};
+      $angulert.warn(alert);
+      expect(alert.classes).toEqual(['warning']);
+    });
+
+    it('should add an error alert', function() {
+      var alert = { message: 'Error alert'};
+      $angulert.error(alert);
+      expect(alert.classes).toEqual(['danger']);
+    });
+
+    it('should add an info alert', function() {
+      var alert = { message: 'Info alert'};
+      $angulert.info(alert);
+      expect(alert.classes).toEqual(['info']);
     });
 
     it('should have a DOM element', function() {
@@ -43,6 +76,4 @@ describe('angulert service', function() {
     });
 
   });
-
-
 });
