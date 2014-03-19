07ac30ab9488521868921a8aeb30276e211c17c0
Ian Shoemaker <iss90g@gmail.com>
2014-03-18 23:03:13 -0400

    service interface tests updateAlert tests deleteAlert tests updated getAlert(s) tests

---
+++ test/unit/angulert-center-spec.js
@@ -0,0 +1,5 @@
+describe('angulert-center', function() {
+
+  beforeEach(module('angulert'));
+  
+});
--- test/unit/service-spec.js
+++ test/unit/service-spec.js
@@ -7,33 +7,20 @@ describe('angulert service', function() {
       expect(angular.isFunction($angulert.setConfig)).toBe(true);
       expect(angular.isFunction($angulert.disable)).toBe(true);
       expect(angular.isFunction($angulert.enable)).toBe(true);
+      expect(angular.isFunction($angulert.success)).toBe(true);
+      expect(angular.isFunction($angulert.warn)).toBe(true);
+      expect(angular.isFunction($angulert.error)).toBe(true);
+      expect(angular.isFunction($angulert.info)).toBe(true);
       expect(angular.isFunction($angulert.addAlert)).toBe(true);
       expect(angular.isFunction($angulert.getAlert)).toBe(true);
-      expect(angular.isFunction($angulert.getAlerts)).toBe(true);
       expect(angular.isFunction($angulert.updateAlert)).toBe(true);
       expect(angular.isFunction($angulert.deleteAlert)).toBe(true);
+      expect(angular.isFunction($angulert.getAlerts)).toBe(true);
       expect(angular.isFunction($angulert.clearAlerts)).toBe(true);
       expect(angular.isFunction($angulert.getHistory)).toBe(true);
       expect(angular.isFunction($angulert.clearHistory)).toBe(true);
     }));
   });
-
-  it('should get alerts', inject(function($injector) {
-    var $angulert = $injector.get('$angulert');
-    $angulert.addAlert({
-      message: 'Alert'
-    });
-    expect($angulert.getAlerts()).toEqual([{message: 'Alert'}]);
-  }));
-
-  it('should clear alerts', inject(function($injector) {
-    var $angulert = $injector.get('$angulert');
-    $angulert.addAlert({
-      message: 'Alert'
-    });
-    $angulert.clearAlerts();
-    expect($angulert.getAlerts().length).toEqual(0);
-  }));
 
   describe('adding alerts', function() {
     var $angulert;
@@ -84,4 +71,122 @@ describe('angulert service', function() {
     });
 
   });
+
+  describe('updating alerts', function() {
+    var $angulert;
+
+    beforeEach(inject(function($injector) {
+      $angulert = $injector.get('$angulert');
+      $angulert.clearAlerts();
+    }));
+
+    it('should not update an alert without an id or passing an id', function() {
+      expect($angulert.updateAlert({})).toBe(null);
+      expect($angulert.getAlerts().length).toBe(0);
+    });
+
+    it('should not update an alert without a matching id', function() {
+      var alert = {
+        message: 'Awesome Alert',
+        _id: '123id123'
+      };
+      $angulert.addAlert(alert);
+      expect($angulert.updateAlert({message:'Different Alert', id:'notthisid'})).toBe(null);
+      expect($angulert.getAlerts().length).toBe(1);
+
+    });
+
+    it('should update an alert given a matching id', function() {
+      var alert = {
+        message: 'Awesome Alert',
+        _id: '123id123'
+      };
+      $angulert.addAlert(alert);
+      alert.message = 'Changed Message';
+      $angulert.updateAlert(alert);
+      expect($angulert.getAlerts().length).toBe(1);
+      expect($angulert.getAlert('123id123').message).toBe('Changed Message');
+    });
+  });
+
+  describe('getting alerts', function() {
+    var $angulert;
+
+    beforeEach(inject(function($injector) {
+      $angulert = $injector.get('$angulert');
+      $angulert.clearAlerts();
+    }));
+
+    it('should get alerts', function() {
+      $angulert.addAlert({
+        message: 'Alert',
+        _id: '123id123'
+      });
+      expect($angulert.getAlerts()).toEqual([{message: 'Alert',_id: '123id123'}]);
+    });
+
+    it('should get an alert by id', function() {
+      $angulert.addAlert({
+        message: 'Alert',
+        _id: '123id123'
+      });
+      expect($angulert.getAlert('123id123')).toEqual({message: 'Alert',_id: '123id123'});
+    });
+
+  });
+
+  describe('clearing alerts', function() {
+    var $angulert;
+
+    beforeEach(inject(function($injector) {
+      $angulert = $injector.get('$angulert');
+      $angulert.clearAlerts();
+    }));
+
+    it('should clear alerts', function() {
+      $angulert.addAlert({
+        message: 'Alert'
+      });
+      $angulert.clearAlerts();
+      expect($angulert.getAlerts().length).toEqual(0);
+    });
+  });
+
+  describe('deleting alerts', function() {
+    var $angulert;
+
+    beforeEach(inject(function($injector) {
+      $angulert = $injector.get('$angulert');
+      $angulert.clearAlerts();
+    }));
+
+    it('should not delete an alert without passing an id', function() {
+      expect($angulert.deleteAlert()).toBe(null);
+      $angulert.addAlert({
+        message: 'Awesome Alert',
+        _id: '123id123'
+      });
+      $angulert.deleteAlert();
+      expect($angulert.getAlerts().length).toBe(1);
+    });
+
+    it('should not delete an alert without a matching id', function() {
+      $angulert.addAlert({
+        message: 'Awesome Alert',
+        _id: '123id123'
+      });
+      $angulert.deleteAlert('dontfindme');
+      expect($angulert.getAlerts().length).toBe(1);
+    });
+
+    it('should delete an alert given a matching id', function() {
+      var id = $angulert.addAlert({
+        message: 'Awesome Alert'
+      });
+      $angulert.deleteAlert(id);
+      expect($angulert.getAlerts().length).toBe(0);
+    });
+
+  });
+
 });
